f5c0b8908782287e4bf46a83c13732d507d44df4
JoeHO888 <joe.hochotai@gmail.com>
2023-02-04 22:08:39 +0800

    Allow 6443 port for k3s

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -102,6 +102,15 @@ resource "aws_security_group_rule" "allow_https_ingress" {
   security_group_id = aws_security_group.security_group.id
 }
 
+resource "aws_security_group_rule" "allow_k8s_api_ingress" {
+  type              = "ingress"
+  from_port         = 6443
+  to_port           = 6443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.security_group.id
+}
+
 resource "aws_security_group_rule" "allow_all_egress" {
   type              = "egress"
   from_port         = 0
